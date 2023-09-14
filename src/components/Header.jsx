20921cb047172be7206e4e7bd5a16a4fef906f16
Eshaan Shetty <eshaanshetty03@gmail.com>
2023-09-14 22:55:10 +0530

    User Auth Integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,71 +1,112 @@
-import React from "react";
+// import React from "react";
 import { FaUmbrellaBeach } from "react-icons/fa";
-import { GiJugglingClubs, GiParkBench } from 'react-icons/gi';
-import { IoMdRestaurant } from 'react-icons/io';
-import { LuHotel } from 'react-icons/lu';
-import { MdOutlineLiveTv, MdOutlineTempleHindu } from 'react-icons/md';
-import { Link } from 'react-router-dom';
+import { GiJugglingClubs, GiParkBench } from "react-icons/gi";
+import { IoMdRestaurant } from "react-icons/io";
+import { LuHotel } from "react-icons/lu";
+import { MdOutlineLiveTv, MdOutlineTempleHindu } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../config.js";
+import Logo from "../assets/icons/TourMyMangalore-logos_black.png";
 
 import "./comp-styles/Header.css";
 
 function Header() {
+  const [signIn, setSignIn] = useState(false);
+  const [signOut, setSignOut] = useState(false);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setSignIn(!signIn);
+        setSignOut(!signOut);
+      } else {
+      }
+    });
+  }, []);
+
+  const userSignOut = () => {
+    auth.signOut();
+    // alert("Logged out, Please refresh the page");
+    window.location.reload(false);
+  };
+
+  const GoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((data) => {
+        const profilePic = data.user.photoURL;
+        localStorage.setItem("profilePic", profilePic);
+      })
+      .catch((err) => {});
+  };
+
   return (
     <>
-      <header>      
-       <h1>TourMyMangalore</h1>
+      <header>
+        <Link to="/">
+          <img src={Logo} alt="" />
+        </Link>
         <nav>
           <ul>
             <li>
-            <FaUmbrellaBeach />
-              <a href="">
-                {" "}
-                  Beaches
-                </Link>
-            </li>
-            <li>
-            <LuHotel />
-              <a href="">
-                {" "}
-                  Hotels
+              {/* <FaUmbrellaBeach /> */}
+              <Link to="/beach">
+                <p>Beaches</p>
               </Link>
             </li>
             <li>
-            <MdOutlineTempleHindu />
-              <a href="">
-                {" "}
-                 Religious Places
+              {/* <LuHotel /> */}
+              <Link to="/hotels">
+                <p>Hotels</p>
               </Link>
             </li>
             <li>
-            <GiParkBench />
-              <a href="">
-                {" "}
-                 Parks
+              {/* <MdOutlineTempleHindu /> */}
+              <Link to="/religious">
+                <p>Religious Places</p>{" "}
               </Link>
             </li>
             <li>
-            <IoMdRestaurant />
-              <a href="">
+              {/* <GiParkBench /> */}
+              <Link to="/parks">
                 {" "}
-                 Restaurants
+                <p>Parks</p>
               </Link>
             </li>
             <li>
-            <GiJugglingClubs />
-              <a href="">
+              {/* <IoMdRestaurant /> */}
+              <Link to="/restaurants">
                 {" "}
-                 Clubs
+                <p>Restaurants</p>
               </Link>
             </li>
             <li>
-            <MdOutlineLiveTv />
-              <a href="">
+              {/* <GiJugglingClubs /> */}
+              <Link to="/clubs">
                 {" "}
-                 <span className="live">Live Events</span>
-             </Link>
+                <p>Clubs</p>
+              </Link>
             </li>
             <li>
-              <button>Sign In</button>
+              {/* <MdOutlineLiveTv /> */}
+              <Link to="/live">
+                {" "}
+                <span className="live">
+                  <p>Live Events!</p>
+                </span>
+              </Link>
+            </li>
+            <li>
+              {signIn ? (
+                <div onClick={userSignOut} className="user-div">
+                  |
+                  <img src={localStorage.getItem("profilePic")} alt="" />
+                  <h2>Logout</h2>
+                </div>
+              ) : (
+                <button onClick={GoogleSignIn}>Sign In</button>
+              )}
             </li>
           </ul>
         </nav>
--- src/components/RecommendedForYou.jsx
+++ src/components/RecommendedForYou.jsx
@@ -12,11 +12,11 @@ function RecommendedForYou() {
     superLargeDesktop: {
       // the naming can be any, depends on you.
       breakpoint: { max: 4000, min: 3000 },
-      items: 5,
+      items: 4,
     },
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 5,
+      items: 4,
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
@@ -32,7 +32,7 @@ function RecommendedForYou() {
       <section className="recommended-for-you-section">
         <h1>Recommended For You</h1>
         <h5>Best recommended places in Mangalore</h5>
-        <Carousel responsive={responsive} style={{margin:"0"}}>
+        <Carousel responsive={responsive}>
           <div className="recommended-cards">
             <img src={hotelimg} />
             <p>Name Of the Recommended</p>
