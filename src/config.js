0dfa50fa1cc3b9520cfeb06a48e8ee20f2659ee5
Eshaan Shetty <eshaanshetty03@gmail.com>
2023-09-26 22:15:19 +0530

    Update

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,5 +23,6 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider(app);
 const analytics = getAnalytics(app);
+const db = firebase.firestore();
 
-export { auth, provider };
+export { auth, provider,db };
