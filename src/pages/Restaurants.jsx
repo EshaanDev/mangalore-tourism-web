1990b57a701b87e72cdf4fb3944c229c1fee7fd7
Eshaan Shetty <eshaanshetty03@gmail.com>
2023-10-19 15:17:47 +0530

    Updated Data

--- src/pages/Restaurants.jsx
+++ src/pages/Restaurants.jsx
@@ -1,139 +1,66 @@
 import React from 'react';
 import { FaRegStar, FaStar } from "react-icons/fa";
 import restaurantimg from "../assets/images/restaurant1.jpg";
+import { GrLocation } from "react-icons/gr";
 import Header from '../components/Header';
 import "../components/comp-styles/Restaurants.css";
+import { Link } from 'react-router-dom';
+import { useState,useEffect } from 'react';
+import { firestore } from "../config";
+import { collection, getDocs } from "firebase/firestore";
 
 function Restaurants() {
+  const [data, setData] = useState([]);
+  // const dataRef = firebase.firestore().collection('hotels')
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const dataCollection = collection(firestore, "restaurants-data");
+        const querySnapshot = await getDocs(dataCollection);
+        const dataCollected = [];
+        querySnapshot.forEach((doc) => {
+          dataCollected.push({ id: doc.id, ...doc.data() });
+        });
+        setData(dataCollected);
+      } catch (error) {
+        console.error("Error Fetching Data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  const beaches = data.map((data) => {
+    return (
+      <Link to="/beachinfo">
+        <div className="beach-card">
+          <div className="image-div">
+            <img src={data.IMAGE} alt="Loading...please wait" />
+          </div>
+          <div className="all-info">
+            <h4 className="place-name">{data.NAME}</h4>
+            <h6 className="place-location">
+              <GrLocation className="location-icon" />
+              {data.LOCATION}
+            </h6>
+          </div>
+        </div>
+      </Link>
+    );
+  });
+
   return (
     <>
-    <Header />
-    <input type="search" className='search-each' id="" placeholder='Search' />
-     <h4 className='top-line'>Our restaurant listings provide comprehensive details, including cuisine types, reviews, and reservation options, empowering you to savor unforgettable moments and indulge in the finest gastronomic delights during your travels.</h4>
-    <section className="restaurants-section">
-          <div className="restaurants-cards">
-            <img src={restaurantimg} />
-            <p>Name Of the Beach</p>
-            <p>Location</p>
-            <p>
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaRegStar />
-            </p>
-          </div>
-          <div className="restaurants-cards">
-            <img src={restaurantimg} />
-            <p>Name Of the Beach</p>
-            <p>Location</p>
-            <p>
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaRegStar />
-            </p>
-          </div>
-          <div className="restaurants-cards">
-            <img src={restaurantimg} />
-            <p>Name Of the Beach</p>
-            <p>Location</p>
-            <p>
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaRegStar />
-            </p>
-          </div>
-          <div className="restaurants-cards">
-            <img src={restaurantimg} />
-            <p>Name Of the Beach</p>
-            <p>Location</p>
-            <p>
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaRegStar />
-            </p>
-          </div>
-          <div className="restaurants-cards">
-            <img src={restaurantimg} />
-            <p>Name Of the Beach</p>
-            <p>Location</p>
-            <p>
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaRegStar />
-            </p>
-          </div>
-          <div className="restaurants-cards">
-            <img src={restaurantimg} />
-            <p>Name Of the Beach</p>
-            <p>Location</p>
-            <p>
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaRegStar />
-            </p>
-          </div>
-          <div className="restaurants-cards">
-            <img src={restaurantimg} />
-            <p>Name Of the Beach</p>
-            <p>Location</p>
-            <p>
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaRegStar />
-            </p>
-          </div>
-          <div className="restaurants-cards">
-            <img src={restaurantimg} />
-            <p>Name Of the Beach</p>
-            <p>Location</p>
-            <p>
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaRegStar />
-            </p>
-          </div>
-          <div className="restaurants-cards">
-            <img src={restaurantimg} />
-            <p>Name Of the Beach</p>
-            <p>Location</p>
-            <p>
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaRegStar />
-            </p>
-          </div>
-          <div className="restaurants-cards">
-            <img src={restaurantimg} />
-            <p>Name Of the Beach</p>
-            <p>Location</p>
-            <p>
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaRegStar />
-            </p>
-          </div>
-      </section>
+      <Header />
+      <input type="search" className="search-each" id="" placeholder="Search" />
+      <h4 className="top-line">
+        From secluded coves to vibrant coastal hubs, our tourism website
+        showcases an array of exquisite beach destinations, ensuring you'll find
+        the ideal seaside escape for relaxation and adventure.
+      </h4>
+      <div className="beaches-cards">{beaches}</div>
     </>
-  )
+  );
+  
 }
 
 export default Restaurants
