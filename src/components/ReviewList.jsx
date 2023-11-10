1ee253b266bf7b4c87b50af9a0ac5de0aa029df6
Eshaan Shetty <eshaanshetty03@gmail.com>
2023-11-10 15:02:23 +0530

    Push

--- src/components/ReviewList.jsx
+++ src/components/ReviewList.jsx
@@ -28,53 +28,49 @@
 
   // export default ReviewList;
 
-import { collection, getDocs, query, where } from 'firebase/firestore';
-import React, { useEffect, useState } from 'react';
-import '../components/comp-styles/ReviewList.css'; // Import your CSS file for styling
-import { firestore } from '../config'; // Import the Firestore instance
-  
-  function ReviewList({beachData}) {
-    const [reviews, setReviews] = useState([]);
-  
-    useEffect(() => {
-      const fetchReviews = async () => {
-        try {
-          if (beachData && beachData.NAME){
-          const reviewsCollection = collection(firestore, 'reviews');
-          const q = query(reviewsCollection, where('place','==',beachData.NAME))
-          const querySnapshot = await getDocs(q);
-          const reviewData = [];
-          querySnapshot.forEach((doc) => {
-            reviewData.push({ id: doc.id, ...doc.data() });
-          });
-          console.log('fetched data ', reviewData)
-          setReviews(reviewData);
-        }
-        } catch (error) {
-          console.error('Error fetching reviews:', error);
-        }
-      };
-      
-      console.log('place name',beachData && beachData.NAME)
-      fetchReviews();
-    }, [beachData]);
-  
-    return (
-      <div className="review-list-container">
-        <h2>Visitors Reviews</h2>
-        <div className="review-list">
-          {reviews.map((review) => (
-            <div key={review.id} className="review-item">
-              <div className="review-header">
-                <strong>{review.userEmail}</strong><br /> (Rating: {review.rating})
+  import { collection, getDocs, query, where } from 'firebase/firestore';
+  import React, { useEffect, useState } from 'react';
+  import '../components/comp-styles/ReviewList.css'; // Import your CSS file for styling
+  import { firestore } from '../config'; // Import the Firestore instance
+    
+    function ReviewList({beachData}) {
+      const [reviews, setReviews] = useState([]);
+    
+      useEffect(() => {
+        const fetchReviews = async () => {
+          try {
+            const reviewsCollection = collection(firestore, 'reviews');
+            const q = query(reviewsCollection, where('place','==',beachData.NAME))
+            const querySnapshot = await getDocs(q);
+            const reviewData = [];
+            querySnapshot.forEach((doc) => {
+              reviewData.push({ id: doc.id, ...doc.data() });
+            });
+            setReviews(reviewData);
+          } catch (error) {
+            console.error('Error fetching reviews:', error);
+          }
+        };
+    
+        fetchReviews();
+      }, []);
+    
+      return (
+        <div className="review-list-container">
+          <h2>Visitors Reviews</h2>
+          <div className="review-list">
+            {reviews.map((review) => (
+              <div key={review.id} className="review-item">
+                <div className="review-header">
+                  <strong>{review.userEmail}</strong><br /> (Rating: {review.rating})
+                </div>
+                <div className="review-comment">{review.review}</div>
               </div>
-              <div className="review-comment">{review.review}</div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-    );
-  }
-  
-  export default ReviewList;
-  
+      );
+    }
+    
+    export default ReviewList;
+    
